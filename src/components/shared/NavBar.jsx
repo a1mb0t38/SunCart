84217777ea6b6cbd76adba709ed6698f038d9d0d
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-05-02 12:33:51 +0530

    added the logout function

--- src/components/shared/NavBar.jsx
+++ src/components/shared/NavBar.jsx
@@ -1,11 +1,19 @@
+"use client";
 import Image from 'next/image';
 import React from 'react';
 import Img from '@/assets/user.png';
 import Link from 'next/link';
 import { TbSunHigh } from 'react-icons/tb';
 import NavLink from './NavLink';
+import { authClient } from '@/lib/auth-client';
 
 const NavBar = () => {
+
+    const {data:session, isPending} = authClient.useSession()
+    // console.log(session, "session")
+    const  user = session?.user;
+
+
     return (
         <div className='flex justify-between items-center bg-linear-to-tr from-blue-700/10 via-transparent to-yellow-700/10 rounded-sm'>
             <div>
@@ -21,11 +29,19 @@ const NavBar = () => {
                     <li><NavLink href={'/profile'}>Profile</NavLink></li>
                 </ul>
             </div>
-            <div className='flex items-center gap-3 p-4'>
-                <span className='text-yellow-700'>Hello</span>
-                <Image src={Img} alt='user image'></Image>
-                <button className='btn text-yellow-800 bg-amber-500/70'><Link href={'/login'}>Login</Link></button>
+
+            <div className='p-4'>
+                {isPending ? <span className="loading loading-spinner text-error"></span> : user ? (<div className='flex items-center gap-3'>
+                <span className='text-yellow-700'>Hello {user?.name}</span>
+                <Image className='rounded-full' src={user?.image || Img} width={50} height={50} alt='user image'></Image>
+                <button className='btn text-yellow-800 bg-amber-500/70' onClick={ async()=> await authClient.signOut()}>logout</button>
+            </div>) : (<button className='btn text-yellow-800 bg-amber-500/70'><Link href={'/login'}>Login</Link></button>) }
             </div>
+            
+                
+                
+                
+        
         </div>
     );
 };
