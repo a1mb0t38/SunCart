cba88b7f340c1bfdaa36377c0addbd44529fb648
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-05-02 12:10:09 +0530

    added the login authentication

--- src/app/(auth)/login/page.jsx
+++ src/app/(auth)/login/page.jsx
@@ -1,9 +1,11 @@
 "use client";
+import { authClient } from '@/lib/auth-client';
 import Link from 'next/link';
 import React, { useState } from 'react';
 import { useForm } from 'react-hook-form';
 import { FaEye, FaEyeSlash } from 'react-icons/fa';
 import { TbSunHigh } from 'react-icons/tb';
+import { toast, ToastContainer } from 'react-toastify';
 
 
 const LoginPage = () => {
@@ -16,7 +18,22 @@ const LoginPage = () => {
         formState: {errors},
     } = useForm();
 
+    const handleLoginFunc = async (data) =>{
+        // console.log(data, "data")
+        const {data:res, error} = await authClient.signIn.email({
+            name: data.name,
+            email: data.email,
+            password: data.password,
+            callbackURL: '/',
+        })
 
+         if(error){
+            toast.error(error.message);
+        }
+        if(res){
+            alert("Wellcome to our news site")
+        }
+    }
 
     return (
         <div className='min-h-[calc(100vh-100px)] flex items-center justify-center bg-linear-to-tr from-blue-700/10 via-transparent to-yellow-700/10 overflow-hidden'>
@@ -31,20 +48,22 @@ const LoginPage = () => {
                 </div>
                 <hr className='text-gray-300' />
                 <div>
-                    <form>
+                    <form onSubmit={handleSubmit(handleLoginFunc)}>
                         <fieldset className="fieldset">
                             <legend className="fieldset-legend">Email</legend>
-                            <input type="email" className="input w-full" placeholder="Enter Your Email" />
-                            
+                            <input type="email" className="input w-full" placeholder="Enter Your Email" {...register("email",{ required: "Email is required" })} />
+                            {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                         </fieldset>
 
                         <fieldset className="fieldset relative">
                             <legend className="fieldset-legend">Password</legend>
-                            <input type={isShowPassword ? "text" : "password"} className="input w-full" placeholder="Enter Your Password" />
+                            <input type={isShowPassword ? "text" : "password"} className="input w-full" placeholder="Enter Your Password" {...register("password",{ required: "Must put your password" })} />
                             <span className='absolute right-1 top-4 mr-2 cursor-pointer' onClick={()=>setisShowPassword(!isShowPassword)}>{isShowPassword ? <FaEye></FaEye> : <FaEyeSlash></FaEyeSlash>}</span>
+                            {errors.email && <p className='text-red-500'>{errors.email.message}</p>}
                         </fieldset>
                         <button className='btn w-full mt-3.5 bg-amber-500/70 font-semibold'>SignIn</button>
                     </form>
+                    <ToastContainer></ToastContainer>
                     <p className='mt-3.5 text-center'>Don't have an account <Link className='text-yellow-500/70' href={'/register'}>Register</Link> </p>
                 </div>
             </div>
