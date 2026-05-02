e5cee9224afaf46ece5a498c36deed53465bcfa1
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-05-02 15:30:19 +0530

    redirected the home to home page route

--- src/app/(main)/page.js
+++ src/app/(main)/page.js
@@ -1,9 +1,7 @@
+
 import Image from "next/image";
+import { redirect } from "next/navigation";
 
 export default function Home() {
-  return (
-     <div>
-      <h2 className="text-red-600">this is home page</h2>
-     </div>
-  );
+  redirect('/home')
 }
