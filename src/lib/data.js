701338ef7232156185292d5c42e623ac78014c7f
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-05-04 12:52:24 +0530

    fixed api url in lib

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,5 @@
 export async function getProducts(){
-    const res = await fetch('http://localhost:3000/data.json');
+    const res = await fetch('/data.json');
     const data = await res.json()
     return data;
 }
