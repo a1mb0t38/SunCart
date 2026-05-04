b6868a7b748f6fc39323aea8eb6094530169473d
mursahaulIslamNoor <mursahaulnur17@gmail.com>
2026-05-04 12:57:57 +0530

    fixed the lib folder api data fetch

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,5 +1,14 @@
-export async function getProducts(){
-    const res = await fetch('/data.json');
-    const data = await res.json()
-    return data;
+// export async function getProducts(){
+//     const res = await fetch('/data.json');
+//     const data = await res.json()
+//     return data;
+// }
+
+import fs from 'fs/promises';
+import path from 'path';
+
+export async function getProducts() {
+    const filePath = path.join(process.cwd(), 'public', 'data.json');
+    const file = await fs.readFile(filePath, 'utf-8');
+    return JSON.parse(file);
 }
